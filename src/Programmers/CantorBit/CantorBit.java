0ad875aeeb97862c09c2ad1c19c7993f2a4e9d2c
chener <49267211+chanosong@users.noreply.github.com>
2023-01-05 02:06:14 +0900

    feat: Finish CantorBit
    ~ CantorBit
    https://school.programmers.co.kr/learn/courses/30/lessons/148652

--- src/Programmers/CantorBit/CantorBit.java
+++ src/Programmers/CantorBit/CantorBit.java
@@ -1,37 +1,47 @@
 package Programmers.CantorBit;
-
+// https://school.programmers.co.kr/learn/courses/30/lessons/148652
 public class CantorBit {
-    static long calculate(long length, long one, long k, long zzl, long zzr) {
+
+    static long calculate(int depth, long k) {
         long cnt = 0;
-
-        k /= 5;
-
-
-
-        return cnt;
+        long share = k / (long) Math.pow(5, depth - 1);
+        long nk = k % (long) Math.pow(5, depth - 1);
+        
+        if (depth == 1) {
+            if (k <= 2)
+            {
+                return k;
+            }
+            else {
+                return k - 1;
+            }
+        }
+        
+        // before zeros
+        if (share <= 2) {
+            cnt = (long) Math.pow(4, depth - 1) * share;
+        }
+        // after zeros
+        else {
+            cnt = (long) Math.pow(4, depth - 1) * (share - 1);
+        }
+        
+        if (share == 2) {
+            return cnt;
+        }
+    
+        return calculate(depth - 1, nk) + cnt;
     }
+
     public static void main(String[] args) {
         int n = 2;
-        long length = 1;
-        long one = 1;
         long l = 4;
         long r = 17;
         long rcnt = 0, lcnt = 0;
-        // zero zone
-        long zzl = length / 5 * 2 + 1;
-        long zzr = length / 5 * 3;
         int answer = 0;
-        String code = "11011";
 
-        for (int i = 0; i < n; i++) {
-            length *= 5;
-            one *= 4;
-            zzl = length / 5 * 2 + 1;
-            zzr = length / 5 * 3;
-        }
-
-        rcnt = calculate(length, one, r, zzl, zzr);
-        lcnt = calculate(length, one, l-1, zzl, zzr);
+        rcnt = calculate(n, r);
+        lcnt = calculate(n, l - 1);
         answer = (int)(rcnt - lcnt);
 
         System.out.println(answer);
