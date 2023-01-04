7c879f4bce890fd05a3c711268ad115c93d89d1d
chener <49267211+chanosong@users.noreply.github.com>
2023-01-04 23:54:07 +0900

    feat: Finish FatigueLevel
    ~ FatigueLevel
    https://school.programmers.co.kr/learn/courses/30/lessons/87946

--- src/Programmers/FatigueLevel/FatigueLevel.java
+++ src/Programmers/FatigueLevel/FatigueLevel.java
@@ -1,41 +1,62 @@
 package Programmers.FatigueLevel;
-
+/*
+ * https://school.programmers.co.kr/learn/courses/30/lessons/87946
+ */
 import java.util.*;
 
 public class FatigueLevel {
     static int length;
-    static int[] output;
     static List<int[]> order = new ArrayList<>();
-    static int[] visited;
+    static boolean[] visited;
+
+    // factorial
     static int fact(int n) {
         if (n <= 1) return n;
         else return fact(n-1) * n;
     }
 
-    static void perm(int cnt) {
+    // permutation
+    static void perm(int[] output, int cnt) {
         if(cnt == length) {
-            order.add(output);
+            int[] cases = new int[length];
+            for (int i = 0; i < length; i++) {
+                cases[i] = output[i];
+            }
+            order.add(cases);
+            return;
         }
         for (int i = 0; i < length; i++) {
-            if (visited[i] == 1) continue;
-            output[cnt] = i;
-            visited[i] = 1;
-            perm(cnt + 1);
-            visited[i] = 0;
+            if (visited[i]) continue;
+            output[cnt] = i + 1;
+            visited[i] = true;
+            perm(output, cnt + 1);
+            visited[i] = false;
         }
     }
     public static void main(String[] args) {
-        int answer = -1;
+        int answer = 0;
         int k = 80;
         int[][] dungeons = {{80,20},{50,40},{30,10}};
 
         length = dungeons.length;
-        output = new int[length];
-        visited = new int[length];
+        int[] output = new int[length];
+        visited = new boolean[length];
+        
+        // permutataion
+        perm(output, 0);
 
-        perm(0);
         for (int[] arr : order) {
-            System.out.println(Arrays.toString(arr));
+            int kprime = k;
+            int ans = 0;
+            for (int i = 0; i < arr.length; i++) {
+                if (kprime < dungeons[arr[i] - 1][0]) {
+                    answer = Math.max(ans, answer);
+                    break;
+                }
+                kprime -= dungeons[arr[i] - 1][1];
+                ans++;
+                if (i == arr.length - 1) answer = Math.max(ans, answer);
+            }
         }
         System.out.println(answer);
     }
