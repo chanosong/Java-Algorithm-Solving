38e0c27ebbe9c10181f6f3dc719bb98c7d9fc015
chener <49267211+chanosong@users.noreply.github.com>
2023-01-21 23:06:00 +0900

    feat: Solve 1 Programmers problem
    ~ ArcheryContest
    https://school.programmers.co.kr/learn/courses/30/lessons/92342

--- src/Programmers/ArcheryContest/ArcheryContest.java
+++ src/Programmers/ArcheryContest/ArcheryContest.java
@@ -1,4 +1,5 @@
 package Programmers.ArcheryContest;
+
 /*
  * https://school.programmers.co.kr/learn/courses/30/lessons/92342
  */
@@ -9,37 +10,80 @@ import java.util.*;
 public class ArcheryContest {
 
     static int max = 0;
+    static int[] memo;
 
     // lion 기준 스코어 계산
     static int calculate(int[] apeach, int[] lion) {
         int scoreGap = 0;
 
         for (int i = 0; i < 11; i++) {
+            // 둘 다 점수가 없는 경우
+            if (apeach[i] == 0 && lion[i] == 0) continue;
             if (apeach[i] >= lion[i]) scoreGap -= 10 - i;
             else scoreGap += 10 - i;
-        }        
+        }
 
         return scoreGap;
     }
 
-    static void dfs(int[] apeach, int[] lion, int depth) {
-        if (calculate(apeach, lion) > 0) {
-            if (max < )
+    // 작은 점수 순으로 갯수 비교, b가 더 작은 것이 많은 경우 true
+    static boolean compare(int[] a, int[] b) {
+        for (int i = 10; i >= 0; i--) {
+            if (a[i] < b[i]) return true;
+            else if (a[i] > b[i]) return false;
         }
+        return false;
+    }
+
+    static void dfs(int[] apeach, int[] lion, int idx, int cnt) {
+        // 스코어 계산
+        int score = calculate(apeach, lion);
+                
+        // 최고점 갱신했을 경우 메모
+        if (max < score) {
+            max = score;
+            memo = lion.clone();
+            memo[10] = cnt;
+        }
+        else if (max == score && max != 0 && compare(memo, lion)) {
+            memo = lion.clone();
+            memo[10] = cnt;
+        }
+
+        // 다 쓴 경우
+        if (cnt == 0) return;
+
+        // 더이상 갈 곳이 없는 경우
+        if (idx == 10) {
+            //lion[10] = cnt;
+            return;
+        }
+
+        // idx 점수에서 따내는 경우
+        // n이 부족한 경우를 생각하여, 이기지못할꺼면 뒤로 보낸다.
+        if (cnt > apeach[idx]) {
+            lion[idx] = apeach[idx] + 1;
+            dfs(apeach, lion, idx + 1, cnt - (apeach[idx] + 1));
+        }
+        
+        // idx 점수에서 포기하는 경우
+        lion[idx] = 0;
+        dfs(apeach, lion, idx + 1, cnt);
     }
 
     public static void main(String[] args) {
-        int n = 5;
-        int[] info = {2,1,1,1,0,0,0,0,0,0,0};
+        int n = 10;
+        int[] info = { 0, 0, 0, 0, 0, 0, 0, 0, 3, 4, 3 };
         int[] answer = new int[11];
-        int[] fail = {-1};
+        int[] fail = { -1 };
+        
+        memo = fail;
+        dfs(info, answer, 0, n);
+        
+        int sum = Arrays.stream(memo).sum();
 
-        for (int i = 10; i >=0; i--) {
-            if (info[i] == 0) {
-                answer[i] = 1;
-            }
-        }
+        //if (sum > 0 && n != sum) memo[10] = n - sum;
 
-        System.out.println(Arrays.toString(answer));
+        System.out.println(Arrays.toString(memo));
     }
 }
