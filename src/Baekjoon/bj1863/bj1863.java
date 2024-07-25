d69dcd244ea94801645c1c74ae35270f338fac1e
Chanho Song <chanosong@naver.com>
2024-07-25 21:32:14 +0900

    Baekjoon: 1863
    ~ 1863 스카이라인 쉬운거 (완료)

--- src/Baekjoon/bj1863/bj1863.java
+++ src/Baekjoon/bj1863/bj1863.java
@@ -17,31 +17,38 @@ public class bj1863 {
 
         n = Integer.parseInt(br.readLine());
 
-        x = new int[n];
-        y = new int[n];
+        x = new int[n + 1];
+        y = new int[n + 1];
 
         Stack<Integer> st = new Stack<>();
 
         int answer = 0;
 
-        for (int i = 0; i < n; i++) {
-            String[] s = br.readLine().split(" ");
+        for (int i = 0; i <= n; i++) {
+            if (i < n) {
+                String[] s = br.readLine().split(" ");
 
-            x[i] = Integer.parseInt(s[0]);
-            y[i] = Integer.parseInt(s[1]);
-
-            if (st.isEmpty() || st.peek() <= y[i]) {
-                st.push(y[i]);
+                x[i] = Integer.parseInt(s[0]);
+                y[i] = Integer.parseInt(s[1]);
             }
-            else {
+
+            // 더 큰 건물이 들어온 경우 이전 건물 정의 종료
+            while (!st.isEmpty() && st.peek() > y[i]) {
                 st.pop();
                 answer++;
             }
+
+            // 같은 높이인 경우 같은 건물 진행
+            if (!st.isEmpty() && st.peek() == y[i]) continue;
+
+            st.push(y[i]);
         }
 
         while(!st.isEmpty()) {
+            if (st.peek() > 0) {
+                answer++;
+            }
             st.pop();
-            answer++;
         }
 
         System.out.println(answer);
