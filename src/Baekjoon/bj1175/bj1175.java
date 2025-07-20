f704f0f56eb95add156bf800a6a0a4320b9caf57
chanho song <chanosong@naver.com>
2025-07-20 20:54:45 +0900

    Baekjoon: 1175
    + 1175 배달 풀이완료

--- src/Baekjoon/bj1175/bj1175.java
+++ src/Baekjoon/bj1175/bj1175.java
@@ -16,7 +16,7 @@ public class bj1175 {
     static int m;
 
     static String[][] map;
-    static int[][][] visited;
+    static int[][][][][] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -28,23 +28,39 @@ public class bj1175 {
         m = Integer.parseInt(st.nextToken());
 
         map = new String[n][m];
-        visited = new int[n][m][4];
+        
+        // x좌표, y좌표, 진입방향, C방문여부, D방문여부
+        visited = new int[n][m][4][2][2];
 
+        // 5차원 배열
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                Arrays.fill(visited[i][j], Integer.MAX_VALUE);
+                for (int k = 0; k < 4; k++) {
+                    for (int l = 0; l < 2; l++) {
+                        for (int m2 = 0; m2 < 2; m2++) {
+                            visited[i][j][k][l][m2] = Integer.MAX_VALUE;
+                        }
+                    }
+                }
             }
         }
 
+        // 시작점 및 C와 D 설정
         int startX = 0, startY = 0;
-
+        boolean isfoundedC = false;
+        
         for (int i = 0; i < n; i++) {
             map[i] = br.readLine().split("");
-
+            
             for (int j = 0; j < m; j++) {
                 if (map[i][j].equals("S")) {
                     startX = i;
                     startY = j;
+                }
+                else if (map[i][j].equals("C")) {
+                    // 두번째 C인 경우 D로 변경
+                    if (isfoundedC) map[i][j] = "D";
+                    isfoundedC = true;
                 }
             }
         }
@@ -55,11 +71,17 @@ public class bj1175 {
         while (!queue.isEmpty()) {
             Trace trace = queue.poll();
 
-            if (trace.x == startX && trace.y == startY) {
-                trace.visited.add(new Point(startX, startY));
+            // C좌표인 경우
+            if (map[trace.x][trace.y].equals("C")) {
+                trace.isVisitedC = 1;
+            }
+            // D좌표인 경우
+            else if (map[trace.x][trace.y].equals("D")) {
+                trace.isVisitedD = 1;
             }
 
-            if (trace.visited.size() == 2) {
+            // C와 D를 모두 방문한 경우 종료
+            if (trace.isVisitedC == 1 && trace.isVisitedD == 1) {
                 answer = trace.distance;
                 break;
             }
@@ -73,6 +95,8 @@ public class bj1175 {
                 int nc = trace.y + dc[i];
                 int nDir = i;
                 int nDistance = trace.distance + 1;
+                int nIsVisitedC = trace.isVisitedC;
+                int nIsVisitedD = trace.isVisitedD;
 
                 // 벗어난 경우 스킵
                 if (nr < 0 || nc < 0 || nr >= n || nc >= m) continue;
@@ -81,15 +105,14 @@ public class bj1175 {
                 if (map[nr][nc].equals("#")) continue;
 
                 // 이미 간 패턴이면 스킵
-                if (visited[nr][nc][nDir] <= nDistance) continue;
+                if (visited[nr][nc][nDir][nIsVisitedC][nIsVisitedD] <= nDistance) continue;
 
-                queue.add(new Trace(nr, nc, nDistance, nDir, trace.visited));
+                queue.add(new Trace(nr, nc, nDistance, nDir, nIsVisitedC, nIsVisitedD));
 
                 // visited 처리
-                visited[trace.x][trace.y][i] = trace.distance;
+                visited[nr][nc][nDir][nIsVisitedC][nIsVisitedD] = trace.distance;
             }
         }
-
         System.out.println(answer);
     }
 }
@@ -99,21 +122,26 @@ class Trace {
     int y;
     int prevDir;
     int distance;
-    Set<Point> visited = new HashSet<>();
+    // 임의로 C와 D로 설정
+    int isVisitedC;
+    int isVisitedD;
 
     public Trace(int x, int y) {
         this.x = x;
         this.y = y;
         distance = 0;
         prevDir = -1;
+        isVisitedC = 0;
+        isVisitedD = 0;
     }
 
-    public Trace(int x, int y, int distance, int prevDir, Set<Point> visited) {
+    public Trace(int x, int y, int distance, int prevDir, int isVisitedC, int isVisitedD) {
         this.x = x;
         this.y = y;
         this.distance = distance;
         this.prevDir = prevDir;
-        this.visited = visited;
+        this.isVisitedC = isVisitedC;
+        this.isVisitedD = isVisitedD;
     }
 }
 
